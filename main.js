978ada25a8ea001f24cce8e6750492df47a11f7a
Carlos Rocha <carlosrocha@mecajun.com>
2021-10-04 16:36:50 -0300

    basic layout

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-import colors from './src/utils/colors.js'
 import AddCss from './src/utils/addCss.js'
-
 AddCss('style.css')
 
-const dvs = document.querySelectorAll('div')
-
-dvs.forEach((item, index) => {
-  item.style.backgroundColor = Object.values(colors)[index]
-})
-
-
-const olaDiv = document.getElementById('ola')
-const olaHeader = document.createElement('h1')
-olaHeader.textContent = 'Olá'
-olaDiv.appendChild(olaHeader)
-
-
-const teste = () =>{
-  alert('sodanodnasdn')
-}
