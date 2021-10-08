d02e095ac5507ce94e28b01249fe7a60836a4142
Carlos Rocha <carlosrocha@mecajun.com>
2021-10-08 00:44:17 -0300

    Working, missing logos and isItUp

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import AddCss from './src/utils/addCss.js'
+import ToolBeltSearch from './src/components/ToolbeltSearch/ToolbeltSearch.js'
 AddCss('style.css')
 
+ToolBeltSearch()
--- src/components/ToolbeltSearch/ToolbeltSearch.js
+++ src/components/ToolbeltSearch/ToolbeltSearch.js
@@ -1,1 +1,62 @@
-console.log('Hello')
+import AddCss from "../../utils/addCss.js";
+
+function searchGoogle(){
+  const input = document.querySelector('.toolbelt-search-input')
+  if(input){
+    window.open(`https://www.google.com/search?q=${input.value}`,'_blank')
+  }
+}
+
+function searchYoutube(){
+  const input = document.querySelector('.toolbelt-search-input')
+  if(input){
+    window.open(`https://www.youtube.com/results?search_query=${input.value}`,'_blank')
+  }
+  
+}
+
+const ToolBeltSearch = () =>{
+  const mainContainer = document.querySelector('.main-container')
+
+  const toolbeltSearchContainer = document.createElement('div')
+  toolbeltSearchContainer.classList.add('toolbelt-search-container')
+
+  const logoContainer = document.createElement('div')
+  logoContainer.classList.add('toolbelt-search-logo-container')
+
+  var bigHeader = document.createElement('h1')
+  bigHeader.textContent = "TOOLBELT"
+  bigHeader.classList.add('search-container-big-header')
+
+  var smallHeader = document.createElement('h2')
+  smallHeader.textContent = "Search"
+  smallHeader.classList.add('search-container-small-header')
+
+  logoContainer.appendChild(bigHeader)
+  logoContainer.appendChild(smallHeader)
+
+  toolbeltSearchContainer.appendChild(logoContainer)
+
+  const input = document.createElement('input')
+  input.classList.add('toolbelt-search-input')
+
+  const buttonsContainer = document.createElement('div')
+  buttonsContainer.classList.add('toolbelt-search-buttons-container')
+
+  var searchGoogleButton = document.createElement('button')
+  searchGoogleButton.innerHTML = "<p>Search G<p>"
+  searchGoogleButton.onclick = searchGoogle
+
+  var searchYoutubeButton = document.createElement('button')
+  searchYoutubeButton.innerHTML = "<p>Search Y<p>"
+  searchYoutubeButton.onclick = searchYoutube
+
+  buttonsContainer.appendChild(searchGoogleButton)
+  buttonsContainer.appendChild(searchYoutubeButton)
+
+  mainContainer.appendChild(toolbeltSearchContainer)
+  mainContainer.appendChild(input)
+  mainContainer.appendChild(buttonsContainer)
+  AddCss('./src/components/ToolBeltSearch/ToolBeltSearch.css')
+}
+export default ToolBeltSearch
