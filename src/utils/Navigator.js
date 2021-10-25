60a0a1dd23318744a98b988c573e9d0c8af87c2d
Carlos Rocha <carlosrocha@mecajun.com>
2021-10-24 23:59:08 -0300

    updating navigator functions

--- src/utils/Navigator.js
+++ src/utils/Navigator.js
@@ -1,34 +1,48 @@
 import ToolBeltSearch, {ToolbeltSearchDestroy} from "../components/ToolbeltSearch/ToolbeltSearch.js"
 import CalendarScreen, {CalendarScreenDestroy} from "../components/CalendarScreen/CalendarScreen.js"
+import PaintScreen, {PaintScreenDestroy} from "../components/PaintScreen/PaintScreen.js"
 
 var CurrentSelectedMenuItem = {
+  id: 'menu-item-search',
   screen: ToolBeltSearch,
   destroy: ToolbeltSearchDestroy,
+}
+
+const NavigateToScreen = (screen, destroy, id) => {
+  CurrentSelectedMenuItem.destroy()
+  document.getElementById(CurrentSelectedMenuItem.id).classList.remove('selected-menu-item')
+  document.getElementById(id).classList.add('selected-menu-item')
+  CurrentSelectedMenuItem = {
+    id,
+    screen,
+    destroy
+  }
+  screen()
 }
 
 const CreateToolbeltMenuListeners = () => {
   const search = document.getElementById('menu-item-search')
   const calendar = document.getElementById('menu-item-calendar')
-  calendar.addEventListener('click', () => {
-    CurrentSelectedMenuItem.destroy()
-    CurrentSelectedMenuItem = {
-      screen: CalendarScreen,
-      destroy: CalendarScreenDestroy
-    }
-    CalendarScreen()
-  })
+  const paint = document.getElementById('menu-item-paint')
+
   search.addEventListener('click', () => {
-    CurrentSelectedMenuItem.destroy()
-    CurrentSelectedMenuItem = {
-      screen: ToolBeltSearch,
-      destroy: ToolbeltSearchDestroy
-    }
-    ToolBeltSearch()
+    NavigateToScreen(ToolBeltSearch, ToolbeltSearchDestroy, 'menu-item-search')
   })
+
+  calendar.addEventListener('click', () => {
+    NavigateToScreen(CalendarScreen, CalendarScreenDestroy, 'menu-item-calendar')
+  })
+
+  paint.addEventListener('click', () => {
+    NavigateToScreen(PaintScreen, PaintScreenDestroy, 'menu-item-paint')
+  })
+
+
 }
 
 const Navigator = () => {
   CreateToolbeltMenuListeners()
+
   // Homepage
   return ToolBeltSearch
 }
